d413b530d68fc99b54a61559009b20d82dec4562
王嘉兴 <wangjiaxing@forchange.tech>
2020-07-09 10:44:40 +0800

    let WhileParsec could analysis

--- WhileParsec.hs
+++ WhileParsec.hs
@@ -5,28 +5,37 @@ import Control.Applicative
 import Data.Functor.Classes
 import Data.List as List
 import Data.Char as Char
+import Data.Map (fromList, Map)
+import Data.Tuple
+import Text.Printf
 
 data Parser a where
-  Chr :: Char -> Parser Char
---  Many :: Parser a -> Parser [a]
-  Or :: Parser a -> Parser a -> Parser a
-  Bind :: Parser b -> (b -> Parser a) -> Parser a
   Pure :: a -> Parser a
+  Chr :: Char -> Parser Char
+  Or :: Parser a -> Parser a -> Parser a
+  Many :: Parser a -> Parser [a]
+  Seq :: Parser a -> Parser b -> Parser (a,b)
+  Map :: (a->b) -> Parser a -> Parser b
 
 instance Functor Parser where
   fmap = liftA
 
 instance Applicative Parser where
-  pure = return 
-  (<*>) = ap
-
-instance Monad Parser where
-  return = Pure
-  (>>=) = Bind
+  pure = Pure
+  (Pure f) <*> p = Map f p
+  (Map f p) <*> q = Map (uncurry f) (Seq p q)
 
 instance Alternative Parser where
   empty = undefined
   (<|>) = Or
+
+instance Show (Parser a) where
+  show (Pure x) = "Pure"
+  show (Chr x) = (show x)
+  show (Or p q) = printf "(%s) <|> (%s)" (show p) (show q)
+  show (Many p) = printf "[%s]" (show p)
+  show (Seq p q) = printf "%s >> %s" (show p) (show q)
+  show (Map f p) = (show p)
 
 parse :: Parser a -> String -> Maybe (a, String)
 parse (Chr x) "" = Nothing
@@ -37,15 +46,36 @@ parse (Chr x) (c:code)
 parse (Or p q) code =
   parse p code <|> parse q code
 
-parse (Bind p f) code = do
+parse (Map f p) code = do
   (x,code') <- parse p code
-  parse (f x) code'
+  return (f x, code')
+
+parse (Seq p q) code = do
+  (x,code') <- parse p code
+  (y,code'') <- parse q code'
+  return ((x,y), code'')
 
 parse (Pure x) code = 
   return (x, code)
 
+parse (Many p) code =
+  parse (many p) code
+
 matchStr :: String -> Parser String
 matchStr str = traverse Chr str
+  
+
+sepBy, sepBy1 :: Parser a -> Parser b -> Parser [a]
+sepBy1 p q = (:) <$> p <*> Many ps
+  where ps = seq <$> q <*> p
+
+sepBy p q = sepBy1 p q <|> Pure []
+
+between :: Parser op -> Parser cl -> Parser a -> Parser a
+between open close p = (\a b c->b) <$> open <*> p <*> close
+
+choice :: [Parser a] -> Parser a
+choice ps = foldr (<|>) empty ps
 
 ------- JSON -------
 
@@ -53,29 +83,27 @@ data Value
  = Boolean Bool
  | StringLit String
  | Array [Value]
+ | Object (Map String Value)
  deriving (Show)
 
-bool = do
-  let true = matchStr "true" >> return True
-  let false = matchStr "false" >> return False
-  Boolean <$> (true <|> false)
+bool = Boolean <$> (true <|> false)
+  where 
+    true = fmap (const True) (matchStr "true")
+    false = fmap (const False) (matchStr "false")
 
-string_lit = do 
-  matchStr "'"
-  as <- many (Chr 'a')
-  matchStr "'"
-  return $ StringLit as
+string = between (Chr '\'') (Chr '\'') (Many (Chr 'a'))
+string_lit = StringLit <$> string
 
-array = do 
-  matchStr "["
-  v0 <- value
-  vs <- many (matchStr "," >> value)
-  matchStr "]"
-  return $ Array (v0:vs)
+array = Array <$> between (Chr '[') (Chr ']') (sepBy value (Chr ','))
+
+pair = (\a b c->(a,c)) <$> string <*> Chr ':' <*> value
+object = (Object . fromList) <$> between (Chr '{') (Chr '}') (sepBy pair (Chr ','))
 
 value :: Parser Value
-value = bool <|> string_lit <|> array
+value = choice [bool, string_lit, array, object]
+
+x = parse value "{'aa':true,'aaa':['aaaa']}"
 
 
-x = parse value "['aaaaa','aaa','aaaaaa']"
+
 
